c78dddbf115ec3ccbed136f8c107966c2d29c8b4
Henok <henokmicrometre@gmail.com>
2024-02-25 10:11:59 +0000

    added state

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,21 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 import './App.css';
 import 'tailwindcss/tailwind.css';
 
 function UploadImage() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div className=" border-solid border-4 border-blue-500 flex  items-center justify-between p-4 shadow">
       <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
         <h2 className="mb-3 text-2xl font-semibold">Upload Image</h2>
+        <button type="submit" onClick={handleClick}>
+          You pressed me {count} times
+        </button>
       </div>
     </div>
   );
