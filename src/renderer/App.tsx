be00d01f0bd5f905004b98a1bef2267513906196
Henok <henokmicrometre@gmail.com>
2024-02-25 09:32:22 +0000

    added grid

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,23 +2,54 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'tailwindcss/tailwind.css';
 
-function AlprPlatesSSe() {
+function AlprPlatesImagesSse() {
   return (
-    <div className=" border-solid border-4 border-red-500 bg-black text-white shadow">
-      <h1>electron-react-boilerplate</h1>
-      <h1>electron-react-boilerplate</h1>
-      <h1>electron-react-boilerplate</h1>
-      <h1>electron-react-boilerplate</h1>
-      <h1>electron-react-boilerplate</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className=" border-solid border-4 border-blue-500 flex  items-center justify-between p-4 shadow">
+      <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className="mb-3 text-2xl font-semibold">Alpr images</h2>
+      </div>
     </div>
   );
 }
+
+function AlprPlatesSse() {
+  return (
+    <div className=" border-solid border-4 border-green-500 flex items-center justify-between p-4 shadow">
+      <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className="mb-3 text-2xl font-semibold">Alpr Plates</h2>
+      </div>
+    </div>
+  );
+}
+function AlprdStream() {
+  return (
+    <div className=" border-solid border-4 border-red-500 flex min-h-screen flex-col items-center justify-between p-24 shadow">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">pages/index.js</code>
+        </p>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]" />
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <AlprPlatesImagesSse />
+        </div>
+
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <AlprPlatesSse />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AlprPlatesSSe />} />
+        <Route path="/" element={<AlprdStream />} />
       </Routes>
     </Router>
   );
